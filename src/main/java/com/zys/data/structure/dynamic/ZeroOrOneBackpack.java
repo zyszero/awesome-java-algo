6ffcaff01df6a6da84b1e7f17329cddd3c960f48
zys <346124796@qq.com>
2019-08-16 00:09:55 +0800

    add a simple algorithm implementation of 0-1 Backpack question

---
+++ src/main/java/com/zys/data/structure/dynamic/ZeroOrOneBackpack.java
@@ -0,0 +1,89 @@
+package com.zys.data.structure.dynamic;
+
+/**
+ * 利用动态规划来求解 0 - 1 背包问题
+ *
+ * @author: zys
+ * @date: 2019/8/13 23:30
+ */
+public class ZeroOrOneBackpack {
+
+    /**
+     * 0 - 1 背包问题
+     *
+     * @param weight 物品重量
+     * @param w      背包的最大承重
+     */
+    public int backpack1(int[] weight, int w) {
+        // 物品个数
+        int n = weight.length;
+        // 状态数组，默认是false
+        boolean[][] states = new boolean[n][w + 1];
+        // 哨兵优化
+        states[0][0] = true;
+        if (weight[0] < w) {
+            states[0][weight[0]] = true;
+        }
+        for (int i = 1; i < n; i++) {
+            // 动态规划，状态转移
+            for (int j = 0; j <= w; j++) {
+                // 不选择把物品放入背包
+                if (states[i - 1][j]) {
+                    states[i][j] = states[i - 1][j];
+                }
+            }
+            for (int j = 0; j <= w - weight[i]; j++) {
+                // 选择把物品放入背包
+                if (states[i - 1][j]) {
+                    states[i][weight[i] + j] = true;
+                }
+            }
+        }
+
+        for (int i = w; i >= 0; i--) {
+            if (states[n - 1][i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * 0 - 1 背包问题，动态规划优化空间复杂度
+     * 核心：去除不累加物品的状态分支
+     * 可以通过调试代码，在重新理一下思路
+     *
+     * @param weight
+     * @param w
+     * @return
+     */
+    public int backpack(int[] weight, int w) {
+        // 状态数组
+        boolean[] states = new boolean[w + 1];
+        // 物品个数
+        int n = weight.length;
+        // 哨兵优化
+        if (weight[0] <= w) {
+            states[weight[0]] = true;
+        }
+        // i 为考察个数
+        for (int i = 1; i < n; ++i) {
+            // 从大到小的原因是为了避免重复计算
+            for (int j = w - weight[i]; j >= 0; j--) {
+                // 选择把第 i 个物品放入背包
+                // 为什么直接 states[j] 就行呢？
+                // 因为完全不放入物品的分支，是可以直接省略
+                // 可以不用二维数组的原因是什么？
+                if (states[j]) {
+                    states[weight[i] + j] = true;
+                }
+            }
+        }
+        for (int i = w; i >= 0; --i) {
+            if (states[i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
--- src/main/java/com/zys/data/structure/recall/ZeroOrOneBackpack.java
+++ src/main/java/com/zys/data/structure/recall/ZeroOrOneBackpack.java
@@ -13,6 +13,7 @@ public class ZeroOrOneBackpack {
     /**
      * 我们有一个背包，背包总的承载重星是Wkg。现在我们有n个物品，每个物品的重量不等，并且不可分割。
      * 我们现在期望选择几件物品，装载到背包中。在不超过背包所能装载重量的前提下，如何让背包中物品的总重量最大？
+     * 回溯算法
      *
      * @param w     背包总的承载重量是Wkg
      * @param n     n个物品
@@ -36,4 +37,58 @@ public class ZeroOrOneBackpack {
             backPack(w, n, i + 1, cw + items[i], items);
         }
     }
+
+    /**
+     *
+     */
+    private int maxW = Integer.MIN_VALUE;
+    /**
+     * 物品重量
+     */
+    private int[] items = new int[]{2, 2, 4, 6, 4};
+    /**
+     * 物品个数
+     */
+    private int n = 5;
+
+    /**
+     * 最大承重量
+     */
+    private int w = 9;
+
+    /**
+     * 备忘录
+     */
+    private boolean[][] men = new boolean[5][10];
+
+    /**
+     *
+     * 使用“备忘录”，可以对回溯算法进行优化
+     *
+     * @param i
+     * @param cw
+     */
+    public void backpack(int i, int cw) {
+        // 当物品考察完毕 或 当前已经装进去的物品重量总和达到背包总的承载重量(终止条件)
+        if (i == n || cw == w) {
+            if (cw > maxWeight) {
+                maxW = cw;
+            }
+            return;
+        }
+        // 符合条件就跳过，表明当前是重复计算
+        if (men[i][cw]) {
+            return;
+        }
+        // 记录f(i, cw)这个状态
+        men[i][cw] = true;
+        // 大问题分解为小问题
+        // 不加入背包
+        backpack(i + 1, cw);
+        if (cw + items[i] < w) {
+            backpack(i + 1, cw + items[i]);
+        }
+    }
+
+
 }
